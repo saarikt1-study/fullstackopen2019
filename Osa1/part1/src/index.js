30a67b8c2120503b85509b5f09656a01b52234a6
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2019-06-05 21:14:15 +0300

    Finish the preliminary text part

--- Osa1/part1/src/index.js
+++ Osa1/part1/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+    return (
+        <div>
+            <p>
+                Hello {props.name}, you are {props.age} years old
+            </p>
+        </div>
+    )
+}
+
+const App = () => {
+    const nimi = 'Pekka'
+    const ika = 10
+
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={nimi} age={ika} />
+        </>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
